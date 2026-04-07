274bdff3c53c52ab4e5d2b23f50075a5103f2282
bntrtm <git.bntrtm@tjbainter.com>
2026-04-06 23:47:21 -0500

    feat: alter shebang lines
    See YSAP Style Guide: "shebang"

--- config.sh
+++ config.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 source "$(dirname "$0")/helpers.sh"
 
 # creates a config file under the expected directory: $HOME/.config/triton/triton.conf
-function write_config_file () {
+function write_config_file() {
   triconf="$HOME/.config/triton/triton.conf"
   if ! test -f "${triconf}"; then
     # make directory, create file
     mkdir -p "$HOME/.config/triton" && touch ${triconf}
     default="$(dirname "$0")/default_triton.conf"
-    cat ${default} > ${triconf};
+    cat ${default} >${triconf}
     echo "Wrote file: ${triconf}"
     user_dir_dotfiles=""
     #askpath "dotfiles" user_dir_dotfiles
